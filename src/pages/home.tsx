f0e3c92bd94686ebc93a0fe602f52b9d535bd219
Misians <artemisiakimberlly@alu.uern.br>
2022-01-12 17:15:09 -0300

    navbar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-<<<<<<< HEAD
+
 import NavBar from "../components/navbar";
-=======
+
 import { Link } from "react-router-dom";
->>>>>>> d4b9e4fa9265d4705262df1ffa8c2d9cd89f7e8d
+
 import '../styles/global.css'
 
 
 function Home(){
     return(
         <div>
-<<<<<<< HEAD
             <NavBar></NavBar>
-=======
-            <Link to="/profile"> Perfil</Link>
->>>>>>> d4b9e4fa9265d4705262df1ffa8c2d9cd89f7e8d
-        
+            <Link to="/profile"> Perfil</Link> 
         </div>
     )
 }
-
-
 export default Home
